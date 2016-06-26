664e9ac313c40ec06271a2c10303f4fbce9050c1
Jul S <jul.samorodova@gmail.com>
2016-06-26 23:21:57 +0300

    project 5. initial commit

---
+++ ABNG/ReportCard.java
@@ -0,0 +1,61 @@
+
+public class ReportCard {
+    private int graduateYear;
+    private String studentFirstName;
+    private String studentLastName;
+
+    public ReportCard (int graduateYear, String studentLastName, String studentFirstName) {
+        graduateYear = this.graduateYear;
+        studentLastName = this.studentLastName;
+        studentFirstName = this.studentFirstName;
+    }
+
+    private int studentGrade;
+    private String classTaken;
+    private String teacherLastname;
+    private int attandanceDays;
+
+    public int getGraduateYear() {
+        return graduateYear;
+    }
+
+    public String getStudentFirstName() {
+        return studentFirstName;
+    }
+
+    public String getStudentLastName() {
+        return studentLastName;
+    }
+
+    public int getStudentGrade() {
+        return studentGrade;
+    }
+
+    public void setStudentGrade(int studentGrade) {
+        this.studentGrade = studentGrade;
+    }
+
+    public String getClassTaken() {
+        return classTaken;
+    }
+
+    public void setClassTaken(String classTaken) {
+        this.classTaken = classTaken;
+    }
+
+    public String getTeacherLastname() {
+        return teacherLastname;
+    }
+
+    public void setTeacherLastname(String teacherLastname) {
+        this.teacherLastname = teacherLastname;
+    }
+
+    public int getAttandanceDays() {
+        return attandanceDays;
+    }
+
+    public void setAttandanceDays(int attandanceDays) {
+        this.attandanceDays = attandanceDays;
+    }
+}
